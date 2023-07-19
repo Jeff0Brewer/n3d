6e02730a4f6ef92715688d2ca899196a24ab26cb
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-07-19 14:52:49 -0600

    move info comp to new file

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,8 @@
 import { FC, useEffect, useState } from 'react'
 import { loadData } from '../lib/data'
-import type { GalaxyData, DataHeaders } from '../lib/data'
+import type { GalaxyData } from '../lib/data'
+import GalaxyInfo from '../components/info'
 import Vis from '../components/vis'
-import styles from '../styles/app.module.css'
 
 const App: FC = () => {
     const [data, setData] = useState<GalaxyData | null>(null)
@@ -22,39 +22,6 @@ const App: FC = () => {
             { data && selected && <GalaxyInfo headers={data.headers} fields={selected} /> }
             { data && <Vis data={data} setSelected={setSelected} /> }
         </main>
-    )
-}
-
-type GalaxyInfoProps = {
-    headers: DataHeaders,
-    fields: Array<string>
-}
-
-const GalaxyInfo: FC<GalaxyInfoProps> = ({ headers, fields }) => {
-    const infoFields = {
-        name: 'Object Name',
-        lng: 'LON',
-        lat: 'LAT',
-        redshift: 'Redshift',
-        hierarchy: 'Hierarchy',
-        'luminosity class': 'Luminosity Class',
-        morphology: 'Galaxy Morphology',
-        'activity type': 'Activity Type',
-        'major diameter': 'SDSS-DR6 r Isophotal 25.0 mag arcsec^-2 Major Diam'
-    }
-    return (
-        <div className={styles.info}>
-            { Object.entries(infoFields).map(([key, value], i) => {
-                const field = fields[headers[value]]
-                if (field) {
-                    return <div className={styles.wrap} key={i}>
-                        <p className={styles.label}>{key}</p>
-                        <p>{field}</p>
-                    </div>
-                }
-                return <></>
-            })}
-        </div>
     )
 }
 
---
+++ src/components/info.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react'
+import type { DataHeaders } from '../lib/data'
+import styles from '../styles/info.module.css'
+
+type GalaxyInfoProps = {
+    headers: DataHeaders,
+    fields: Array<string>
+}
+
+const INFO_FIELDS = {
+    name: 'Object Name',
+    lng: 'LON',
+    lat: 'LAT',
+    redshift: 'Redshift',
+    hierarchy: 'Hierarchy',
+    'luminosity class': 'Luminosity Class',
+    morphology: 'Galaxy Morphology',
+    'activity type': 'Activity Type',
+    'major diameter': 'SDSS-DR6 r Isophotal 25.0 mag arcsec^-2 Major Diam'
+}
+
+const GalaxyInfo: FC<GalaxyInfoProps> = ({ headers, fields }) => {
+    return (
+        <div className={styles.info}>
+            { Object.entries(INFO_FIELDS).map(([key, value], i) => {
+                const field = fields[headers[value]]
+                return <div className={field ? styles.wrap : styles.hidden} key={i}>
+                    <p className={styles.label}>{key}</p>
+                    <p>{field}</p>
+                </div>
+            }
+            )}
+        </div>
+    )
+}
+
+export default GalaxyInfo
