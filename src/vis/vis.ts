a6aa64fd84d81cc0f93e46d908a727303caa6836
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-08-01 15:49:03 -0600

    change default eye position

--- src/vis/vis.ts
+++ src/vis/vis.ts
@@ -32,7 +32,7 @@ class VisRenderer {
 
         this.model = mat4.create()
 
-        const eye = vec3.fromValues(7.0, 0.0, 0.0)
+        const eye = vec3.fromValues(0.0, 7.0, 0.0)
         const focus = vec3.fromValues(0, 0, 0)
         const up = vec3.fromValues(0, 0, 1)
         this.view = mat4.lookAt(mat4.create(), eye, focus, up)
