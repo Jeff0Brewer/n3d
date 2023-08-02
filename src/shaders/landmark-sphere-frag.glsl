d4b030df281934eae13ae0808a22fb7a01090425
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-08-02 14:37:29 -0600

    tune lighting

--- src/shaders/landmark-sphere-frag.glsl
+++ src/shaders/landmark-sphere-frag.glsl
@@ -5,7 +5,7 @@ varying vec3 vNormal;
 void main() {
     vec3 lightDir = vec3(0.0, 0.0, 1.0);
     float directional = clamp(dot(vNormal, lightDir), 0.0, 1.0);
-    float ambient = 0.1;
+    float ambient = 0.2;
     float shade = clamp(directional + ambient, 0.0, 1.0);
 
     vec3 color = vec3(1.0, 1.0, 1.0);
